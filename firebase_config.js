62f0f47ea5180d3a9e13ec62edaa0250cd42b176
flavioabdala <flavioabdala@yahoo.com.br>
2025-07-08 12:03:36 -0300

    Add files via upload

---
+++ firebase_config.js
@@ -0,0 +1,12 @@
+// Firebase configuration for your web app
+const firebaseConfig = {
+  apiKey: "AIzaSyDTxRQdMy4QugmySVeSErf5Kqihn_4OhWw",
+  authDomain: "gcc-contratos.firebaseapp.com",
+  projectId: "gcc-contratos",
+  storageBucket: "gcc-contratos.firebasestorage.app",
+  messagingSenderId: "405415511927",
+  appId: "1:405415511927:web:4612190119302726820372",
+  measurementId: "G-T1T4N9WD5T"
+};
+
+export default firebaseConfig;
